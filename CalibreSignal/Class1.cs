0ea76e12aac1ca51a292fe7bba26801e1dba6936
gurkansonmez <gurkansonmez@informascope.com>
2023-01-27 14:59:33 +0300

    new3

---
+++ CalibreSignal/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace CalibreSignal
+{
+	public class Notification<T>
+	{
+
+		public string TransactionId
+		{
+			get; set;
+		}
+		public T Object
+		{
+			get; set;
+		}
+		List<string> errors = new();
+		public List<string> Errors
+		{
+			get => errors;
+		}
+	}
+}
---
+++ SignalRClientConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+namespace SignalRClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/chatHub")
+                .Build();
+
+            connection.StartAsync().Wait();
+            connection.InvokeAsync("SendMessage", arg1: new[] { "Scribo", "Hello" });
+            connection.On("ReceiveMessage", (string userName, string message) =>
+            {
+                Console.WriteLine(userName + ':' + message);
+            });
+        }
+    }
+}
